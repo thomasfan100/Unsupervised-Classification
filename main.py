b8e8cff0626d53f9983a06631527b9cb357eadc8
Thomas Fan <thomasfan100@gmail.com>
2024-04-28 18:42:19 -0500

    pain

---
+++ main.py
@@ -0,0 +1,372 @@
+import argparse
+import os
+import torch
+import numpy as np
+from utils.config import create_config
+from utils.common_config import get_train_dataset, get_train_transformations,\
+                                get_val_dataset, get_val_transformations,\
+                                get_train_dataloader, get_val_dataloader,\
+                                get_optimizer, get_model, adjust_learning_rate,\
+                                get_criterion
+from utils.ema import EMA
+from utils.evaluate_utils import get_predictions, hungarian_evaluate, scan_evaluate
+from utils.evaluate_utils import contrastive_evaluate
+from utils.train_utils import selflabel_train
+from termcolor import colored
+from utils.memory import MemoryBank
+from utils.train_utils import simclr_train
+from utils.utils import fill_memory_bank
+from utils.train_utils import scan_train
+
+# Parser
+parser = argparse.ArgumentParser(description='scan')
+parser.add_argument('--config_env',
+                    help='Config file for the environment')
+parser.add_argument('--config_exp',
+                    help='Config file for the experiment')
+parser.add_argument('--config_env2',
+                    help='Config file for the environment')
+parser.add_argument('--config_exp2',
+                    help='Config file for the experiment')
+parser.add_argument('--config_env3',
+                    help='Config file for the environment')
+parser.add_argument('--config_exp3',
+                    help='Config file for the experiment')
+args = parser.parse_args()
+
+def main():
+    ############################################################################################
+    ## SIMCLR                                                                                 ##
+    ############################################################################################
+    # Retrieve config file
+    p = create_config(args.config_env, args.config_exp)
+    print(colored(p, 'red'))
+    
+    # Model
+    print(colored('Retrieve model', 'blue'))
+    model = get_model(p)
+    print('Model is {}'.format(model.__class__.__name__))
+    print('Model parameters: {:.2f}M'.format(sum(p.numel() for p in model.parameters()) / 1e6))
+    print(model)
+    model = model.cuda()
+   
+    # CUDNN
+    print(colored('Set CuDNN benchmark', 'blue')) 
+    torch.backends.cudnn.benchmark = True
+    
+    # Dataset
+    print(colored('Retrieve dataset', 'blue'))
+    train_transforms = get_train_transformations(p)
+    print('Train transforms:', train_transforms)
+    val_transforms = get_val_transformations(p)
+    print('Validation transforms:', val_transforms)
+    train_dataset = get_train_dataset(p, train_transforms, to_augmented_dataset=True,
+                                        split='train+unlabeled') # Split is for stl-10
+    val_dataset = get_val_dataset(p, val_transforms) 
+    train_dataloader = get_train_dataloader(p, train_dataset)
+    val_dataloader = get_val_dataloader(p, val_dataset)
+    print('Dataset contains {}/{} train/val samples'.format(len(train_dataset), len(val_dataset)))
+    
+    # Memory Bank
+    print(colored('Build MemoryBank', 'blue'))
+    base_dataset = get_train_dataset(p, val_transforms, split='train') # Dataset w/o augs for knn eval
+    base_dataloader = get_val_dataloader(p, base_dataset) 
+    memory_bank_base = MemoryBank(len(base_dataset), 
+                                p['model_kwargs']['features_dim'],
+                                p['num_classes'], p['criterion_kwargs']['temperature'])
+    memory_bank_base.cuda()
+    memory_bank_val = MemoryBank(len(val_dataset),
+                                p['model_kwargs']['features_dim'],
+                                p['num_classes'], p['criterion_kwargs']['temperature'])
+    memory_bank_val.cuda()
+
+    # Criterion
+    print(colored('Retrieve criterion', 'blue'))
+    criterion = get_criterion(p)
+    print('Criterion is {}'.format(criterion.__class__.__name__))
+    criterion = criterion.cuda()
+
+    # Optimizer and scheduler
+    print(colored('Retrieve optimizer', 'blue'))
+    optimizer = get_optimizer(p, model)
+    print(optimizer)
+ 
+    # Checkpoint
+    if os.path.exists(p['pretext_checkpoint']):
+        print(colored('Restart from checkpoint {}'.format(p['pretext_checkpoint']), 'blue'))
+        checkpoint = torch.load(p['pretext_checkpoint'], map_location='cpu')
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        model.load_state_dict(checkpoint['model'])
+        model.cuda()
+        start_epoch = checkpoint['epoch']
+
+    else:
+        print(colored('No checkpoint file at {}'.format(p['pretext_checkpoint']), 'blue'))
+        start_epoch = 0
+        model = model.cuda()
+    
+    # Training
+    print(colored('Starting main loop', 'blue'))
+    for epoch in range(start_epoch, p['epochs']):
+        print(colored('Epoch %d/%d' %(epoch, p['epochs']), 'yellow'))
+        print(colored('-'*15, 'yellow'))
+
+        # Adjust lr
+        lr = adjust_learning_rate(p, optimizer, epoch)
+        print('Adjusted learning rate to {:.5f}'.format(lr))
+        
+        # Train
+        print('Train ...')
+        simclr_train(train_dataloader, model, criterion, optimizer, epoch)
+
+        # Fill memory bank
+        print('Fill memory bank for kNN...')
+        fill_memory_bank(base_dataloader, model, memory_bank_base)
+
+        # Evaluate (To monitor progress - Not for validation)
+        print('Evaluate ...')
+        top1 = contrastive_evaluate(val_dataloader, model, memory_bank_base)
+        print('Result of kNN evaluation is %.2f' %(top1)) 
+        
+        # Checkpoint
+        print('Checkpoint ...')
+        torch.save({'optimizer': optimizer.state_dict(), 'model': model.state_dict(), 
+                    'epoch': epoch + 1}, p['pretext_checkpoint'])
+
+    # Save final model
+    torch.save(model.state_dict(), p['pretext_model'])
+
+    # Mine the topk nearest neighbors at the very end (Train) 
+    # These will be served as input to the SCAN loss.
+    print(colored('Fill memory bank for mining the nearest neighbors (train) ...', 'blue'))
+    fill_memory_bank(base_dataloader, model, memory_bank_base)
+    topk = 20
+    print('Mine the nearest neighbors (Top-%d)' %(topk)) 
+    indices, acc = memory_bank_base.mine_nearest_neighbors(topk)
+    print('Accuracy of top-%d nearest neighbors on train set is %.2f' %(topk, 100*acc))
+    np.save(p['topk_neighbors_train_path'], indices)   
+
+   
+    # Mine the topk nearest neighbors at the very end (Val)
+    # These will be used for validation.
+    print(colored('Fill memory bank for mining the nearest neighbors (val) ...', 'blue'))
+    fill_memory_bank(val_dataloader, model, memory_bank_val)
+    topk = 5
+    print('Mine the nearest neighbors (Top-%d)' %(topk)) 
+    indices, acc = memory_bank_val.mine_nearest_neighbors(topk)
+    print('Accuracy of top-%d nearest neighbors on val set is %.2f' %(topk, 100*acc))
+    np.save(p['topk_neighbors_val_path'], indices)   
+
+    ############################################################################################
+    ## SCAN                                                                                   ##
+    ############################################################################################
+    p = create_config(args.config_env2, args.config_exp2)
+    print(colored(p, 'red'))
+
+    # CUDNN
+    torch.backends.cudnn.benchmark = True
+
+    # Data
+    print(colored('Get dataset and dataloaders', 'blue'))
+    train_transformations = get_train_transformations(p)
+    val_transformations = get_val_transformations(p)
+    train_dataset = get_train_dataset(p, train_transformations, 
+                                        split='train', to_neighbors_dataset = True)
+    val_dataset = get_val_dataset(p, val_transformations, to_neighbors_dataset = True)
+    train_dataloader = get_train_dataloader(p, train_dataset)
+    val_dataloader = get_val_dataloader(p, val_dataset)
+    print('Train transforms:', train_transformations)
+    print('Validation transforms:', val_transformations)
+    print('Train samples %d - Val samples %d' %(len(train_dataset), len(val_dataset)))
+    
+    # Model
+    print(colored('Get model', 'blue'))
+    model = get_model(p, p['pretext_model'])
+    print(model)
+    model = torch.nn.DataParallel(model)
+    model = model.cuda()
+
+    # Optimizer
+    print(colored('Get optimizer', 'blue'))
+    optimizer = get_optimizer(p, model, p['update_cluster_head_only'])
+    print(optimizer)
+    
+    # Warning
+    if p['update_cluster_head_only']:
+        print(colored('WARNING: SCAN will only update the cluster head', 'red'))
+
+    # Loss function
+    print(colored('Get loss', 'blue'))
+    criterion = get_criterion(p) 
+    criterion.cuda()
+    print(criterion)
+
+    # Checkpoint
+    if os.path.exists(p['scan_checkpoint']):
+        print(colored('Restart from checkpoint {}'.format(p['scan_checkpoint']), 'blue'))
+        checkpoint = torch.load(p['scan_checkpoint'], map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])        
+        start_epoch = checkpoint['epoch']
+        best_loss = checkpoint['best_loss']
+        best_loss_head = checkpoint['best_loss_head']
+
+    else:
+        print(colored('No checkpoint file at {}'.format(p['scan_checkpoint']), 'blue'))
+        start_epoch = 0
+        best_loss = 1e4
+        best_loss_head = None
+ 
+    # Main loop
+    print(colored('Starting main loop', 'blue'))
+
+    for epoch in range(start_epoch, p['epochs']):
+        print(colored('Epoch %d/%d' %(epoch+1, p['epochs']), 'yellow'))
+        print(colored('-'*15, 'yellow'))
+
+        # Adjust lr
+        lr = adjust_learning_rate(p, optimizer, epoch)
+        print('Adjusted learning rate to {:.5f}'.format(lr))
+
+        # Train
+        print('Train ...')
+        scan_train(train_dataloader, model, criterion, optimizer, epoch, p['update_cluster_head_only'])
+
+        # Evaluate 
+        print('Make prediction on validation set ...')
+        predictions = get_predictions(p, val_dataloader, model)
+
+        print('Evaluate based on SCAN loss ...')
+        scan_stats = scan_evaluate(predictions)
+        print(scan_stats)
+        lowest_loss_head = scan_stats['lowest_loss_head']
+        lowest_loss = scan_stats['lowest_loss']
+       
+        if lowest_loss < best_loss:
+            print('New lowest loss on validation set: %.4f -> %.4f' %(best_loss, lowest_loss))
+            print('Lowest loss head is %d' %(lowest_loss_head))
+            best_loss = lowest_loss
+            best_loss_head = lowest_loss_head
+            torch.save({'model': model.module.state_dict(), 'head': best_loss_head}, p['scan_model'])
+
+        else:
+            print('No new lowest loss on validation set: %.4f -> %.4f' %(best_loss, lowest_loss))
+            print('Lowest loss head is %d' %(best_loss_head))
+
+        print('Evaluate with hungarian matching algorithm ...')
+        clustering_stats = hungarian_evaluate(lowest_loss_head, predictions, compute_confusion_matrix=False)
+        print(clustering_stats)     
+
+        # Checkpoint
+        print('Checkpoint ...')
+        torch.save({'optimizer': optimizer.state_dict(), 'model': model.state_dict(), 
+                    'epoch': epoch + 1, 'best_loss': best_loss, 'best_loss_head': best_loss_head},
+                     p['scan_checkpoint'])
+    
+    # Evaluate and save the final model
+    print(colored('Evaluate best model based on SCAN metric at the end', 'blue'))
+    model_checkpoint = torch.load(p['scan_model'], map_location='cpu')
+    model.module.load_state_dict(model_checkpoint['model'])
+    predictions = get_predictions(p, val_dataloader, model)
+    clustering_stats = hungarian_evaluate(model_checkpoint['head'], predictions, 
+                            class_names=val_dataset.dataset.classes, 
+                            compute_confusion_matrix=True, 
+                            confusion_matrix_file=os.path.join(p['scan_dir'], 'confusion_matrix.png'))
+    print(clustering_stats)         
+    ############################################################################################
+    ## Self Label                                                                             ##
+    ############################################################################################
+    p = create_config(args.config_env3, args.config_exp3)
+    print(colored(p, 'red'))
+
+    # Get model
+    print(colored('Retrieve model', 'blue'))
+    model = get_model(p, p['scan_model'])
+    print(model)
+    model = torch.nn.DataParallel(model)
+    model = model.cuda()
+
+    # Get criterion
+    print(colored('Get loss', 'blue'))
+    criterion = get_criterion(p)
+    criterion.cuda()
+    print(criterion)
+
+    # CUDNN
+    print(colored('Set CuDNN benchmark', 'blue')) 
+    torch.backends.cudnn.benchmark = True
+
+    # Optimizer
+    print(colored('Retrieve optimizer', 'blue'))
+    optimizer = get_optimizer(p, model)
+    print(optimizer)
+
+    # Dataset
+    print(colored('Retrieve dataset', 'blue'))
+    
+    # Transforms 
+    strong_transforms = get_train_transformations(p)
+    val_transforms = get_val_transformations(p)
+    train_dataset = get_train_dataset(p, {'standard': val_transforms, 'augment': strong_transforms},
+                                        split='train', to_augmented_dataset=True) 
+    train_dataloader = get_train_dataloader(p, train_dataset)
+    val_dataset = get_val_dataset(p, val_transforms) 
+    val_dataloader = get_val_dataloader(p, val_dataset)
+    print(colored('Train samples %d - Val samples %d' %(len(train_dataset), len(val_dataset)), 'yellow'))
+
+    # Checkpoint
+    if os.path.exists(p['selflabel_checkpoint']):
+        print(colored('Restart from checkpoint {}'.format(p['selflabel_checkpoint']), 'blue'))
+        checkpoint = torch.load(p['selflabel_checkpoint'], map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])        
+        start_epoch = checkpoint['epoch']
+
+    else:
+        print(colored('No checkpoint file at {}'.format(p['selflabel_checkpoint']), 'blue'))
+        start_epoch = 0
+
+    # EMA
+    if p['use_ema']:
+        ema = EMA(model, alpha=p['ema_alpha'])
+    else:
+        ema = None
+
+    # Main loop
+    print(colored('Starting main loop', 'blue'))
+    
+    for epoch in range(start_epoch, p['epochs']):
+        print(colored('Epoch %d/%d' %(epoch+1, p['epochs']), 'yellow'))
+        print(colored('-'*10, 'yellow'))
+
+        # Adjust lr
+        lr = adjust_learning_rate(p, optimizer, epoch)
+        print('Adjusted learning rate to {:.5f}'.format(lr))
+
+        # Perform self-labeling 
+        print('Train ...')
+        selflabel_train(train_dataloader, model, criterion, optimizer, epoch, ema=ema)
+
+        # Evaluate (To monitor progress - Not for validation)
+        print('Evaluate ...')
+        predictions = get_predictions(p, val_dataloader, model)
+        clustering_stats = hungarian_evaluate(0, predictions, compute_confusion_matrix=False) 
+        print(clustering_stats)
+        
+        # Checkpoint
+        print('Checkpoint ...')
+        torch.save({'optimizer': optimizer.state_dict(), 'model': model.state_dict(), 
+                    'epoch': epoch + 1}, p['selflabel_checkpoint'])
+        torch.save(model.module.state_dict(), p['selflabel_model'])
+    
+    # Evaluate and save the final model
+    print(colored('Evaluate model at the end', 'blue'))
+    predictions = get_predictions(p, val_dataloader, model)
+    clustering_stats = hungarian_evaluate(0, predictions, 
+                                class_names=val_dataset.classes,
+                                compute_confusion_matrix=True,
+                                confusion_matrix_file=os.path.join(p['selflabel_dir'], 'confusion_matrix.png'))
+    print(clustering_stats)
+    torch.save(model.module.state_dict(), p['selflabel_model'])
+if __name__ == '__main__':
+    main()
